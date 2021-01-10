362c66fc505308a14a9a6ace7ef3c005795fc297
junyoung-sim <eden920.dev@gmail.com>
2021-01-10 09:48:33 -0500

    10 Jan 2021

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="futures",
     description="Deep learning based financial modeling",
+    version="1.0",
     author="Junyoung Sim",
     author_email="eden920.dev@gmail.com",
     url="https://github.com/junyoung-sim/futures/",
