3d07b01f0ee38250d2f48b3c3830335b867514ab
junyoung-sim <eden920.dev@gmail.com>
2021-01-10 07:20:56 -0500

    10 Jan 2021

--- setup.py
+++ setup.py
@@ -4,17 +4,11 @@ from setuptools import setup, find_packages
 setup(
     name="futures",
     description="Deep learning based financial modeling",
-    version="1.0",
+    version="1.1",
     author="Junyoung Sim",
     author_email="eden920.dev@gmail.com",
     url="https://github.com/junyoung-sim/futures/",
     python_requires=">=3.8",
     scripts=["futures.py"],
     packages=find_packages(),
-    install_requires=[
-        "pandas-datareader",
-        "matplotlib",
-        "tqdm",
-        "tensorflow"
-    ]
 )
