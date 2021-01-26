7eb94d8b561d2e2b9a8ed44c64ba0d6398170ec2
junyoung-sim <eden920.dev@gmail.com>
2021-01-26 10:27:55 -0500

    26 Jan 2021

--- beta/src/futures.cpp
+++ beta/src/futures.cpp
@@ -148,7 +148,46 @@ void Futures::encode() {
     vector<vector<float>> input = dataset[5068].get_input();
     for(unsigned int l = 0; l < layer.size(); l++) {
         // padding (optional; check layer attributes)
+        if(get<PADDING>(layer[l].get_attributes()) == true) {
+            vector<vector<float>> pad;
+            for(unsigned int i = 0; i < input.size() + 2; i++) {
+                pad.push_back(vector<float>(input.size() + 2, 0.00));
+            }
+            for(unsigned int i = 1; i < pad.size() - 1; i++) {
+                for(unsigned int j = 1; j < pad[i].size() - 1; j++) {
+                    pad[i][j] =  input[i-1][j-1];
+                }
+            }
+            input = pad;
+        }
         // convolution
+        vector<vector<float>> convolved;
+        vector<vector<float>> kernel = layer[l].get_kernel();
+        for(unsigned int r = 0; r <= input.size() - kernel.size(); r++) {
+            vector<float> row;
+            for(unsigned int c = 0; c <= input.size() - kernel[0].size(); c++) {
+                float matmul = 0.00;
+                unsigned int k_row, k_col = 0;
+                for(unsigned int i = r; i < r + kernel.size(); i++) {
+                    for(unsigned int j = c; j < c + kernel[0].size(); j++) {
+                        matmul += input[i][j] * kernel[k_row][k_col];
+                        k_col++;
+                    }
+                    k_row++;
+                    k_col = 0;
+                }
+                k_row = 0;
+                row.push_back(matmul < 0.00 ? 0.00 : matmul); // ReLU thresholding
+                matmul = 0.00;
+            }
+            convolved.push_back(row);
+        }
+        for(unsigned int i = 0; i < convolved.size(); i++) {
+            for(unsigned int j = 0 ; j < convolved[i].size(); j++) {
+                cout << convolved[i][j] << " ";
+            }
+            cout << endl;
+        }
         // pooling
     }
 }
