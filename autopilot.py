deaaab53b10971f4c7f8e878a8adfa47444726cc
junyoung-sim <eden920.dev@gmail.com>
2021-02-18 10:11:57 -0500

    18 Feb 2021

--- autopilot.py
+++ autopilot.py
@@ -6,7 +6,11 @@ from datetime import datetime, timedelta
 end_time   = sys.argv[1] # hh:mm:00
 interval   = timedelta(minutes=int(sys.argv[2])) # in minutes
 
-while str(datetime.now().time())[:8] != end_time:
+current = str(datetime.now().time())[:8]
+while datetime.strptime(current, "%H:%M:%S") - datetime.strptime(end_time, "%H:%M:%S") < timedelta(seconds=0):
     os.system("./scripts/run")
     time.sleep(interval*60)
+    current = str(datetime.now().time())[:8]
+
+os.system("clear && ./scripts/run")
 
