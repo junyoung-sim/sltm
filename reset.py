3ed05dd07878b775fedc0514a4f29df71576b6ec
junyoung-sim <eden920.dev@gmail.com>
2021-02-26 09:47:00 +0900

    26 Feb 2021

--- reset.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-for model in os.listdir("./models"):
-    if model != ".DS_Store":
-        os.system("rm ./models/" + model + "/res/npy/*.npy")
-        os.system("rm ./models/" + model + "/res/prediction/*.png")
-        os.system("rm ./models/" + model + "/res/validation/*.png")
